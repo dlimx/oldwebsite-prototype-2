37775190a728e5ad6e04abd9c90010186b5d945c
David Li <david.minxiao.li@gmail.com>
2018-11-06 22:01:33 -0500

    add basic webpack setup

---
+++ client/index.js
@@ -0,0 +1,6 @@
+const helloWorld = 'helloWorld!';
+
+console.log('my first test');
+
+export default helloWorld;
+
---
+++ scripts/webpack.client.js
@@ -0,0 +1,82 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: ['./client/index.js'],
+
+  output: {
+    path: path.join(__dirname, '..', 'public'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'public/img/[name].[ext]',
+            outputPath: 'dist/img/'
+          }
+        }
+      },
+
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { minimize: true } },
+            'sass-loader'
+          ]
+        })
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            minimize: true
+          }
+        }
+      },
+      {
+        test: /\.(otf|ttf|eot|woff|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'public/fonts/[name].[ext]',
+          outputPath: 'dist/fonts'
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new ExtractTextPlugin({ filename: 'style.css' }),
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      filename: 'index.html',
+      hash: true
+    })
+  ],
+
+  devServer: {
+    historyApiFallback: true,
+    publicPath: '/',
+    contentBase: './public'
+  }
+};
